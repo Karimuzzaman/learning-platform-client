5157665a4eb70d6564c3d16480501716b90aaf69
Karimuzzaman <pialkarimuzzaman@gmail.com>
2022-10-26 21:30:57 +0600

    4th  question added

--- src/Pages/Shared/Blog/Blog.js
+++ src/Pages/Shared/Blog/Blog.js
@@ -41,7 +41,7 @@ const Blog = () => {
                 </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="3">
-                <Accordion.Header>Accordion Item #2</Accordion.Header>
+                <Accordion.Header>What is Node? How does Node Work?</Accordion.Header>
                 <Accordion.Body>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
